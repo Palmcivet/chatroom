0c5d176a62ab9469eec01737cd8043cfe748c9ff
Palm Civet <palmcivet@MacBook-Pro.local>
2019-09-01 22:51:53 +0800

    design a structure

--- snake/src/Components/Game.jsx
+++ snake/src/Components/Game.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Config } from './Config'
-import Snake from './Components/Snake'
+import { Config } from '../Config'
+import Snake from './Snake'
 
 let init = {
     id: "3d4ca1",
@@ -24,6 +24,10 @@ function Palyer(snake) {
     this.delPlayer = (targetSnake) => (
         pass
     )
+    /**
+     * Player.start() 启动之后定时器不断重复
+     * 常规移动，每次移动进行判断：检测是否撞墙或重合
+     */
     this.start = () => (
         setInterval(
             () => (
@@ -36,7 +40,10 @@ function Palyer(snake) {
     )
 
     /**
-     * 移动检测
+     * 移动检测，有如下步骤：
+     * 1. 移动，返回头部位置
+     * 2. 根据头部位置进行判断
+     * 3. 地图上进行相应绘制
      */
     let moveJudge = (player) => {
 
